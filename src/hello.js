410f3af704746734aea36e9bdc2c95770096f949
padmalakum <92623347+padmalakum@users.noreply.github.com>
2023-05-16 16:00:33 -0700

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Padmaja!",
+        message: "Hello From PadmajaLakum!",
       },
       null,
       2
